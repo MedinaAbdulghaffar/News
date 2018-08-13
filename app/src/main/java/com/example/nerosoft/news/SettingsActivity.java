97c1484dfbc358620eed921eda088d874df8b958
medna <medina.abdulghaffar@gmail.com>
2018-08-13 13:38:11 +0300

    last one

--- app/src/main/java/com/example/nerosoft/news/SettingsActivity.java
+++ app/src/main/java/com/example/nerosoft/news/SettingsActivity.java
@@ -10,7 +10,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
-
+  //last one
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
